33f6929f5e5cadfcff7d7c4404e1d318efb001a1
Ryan Jeremy Pridgen <catscratch8809@gmail.com>
2025-07-03 14:31:45 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    cloudflare = {
+      source = "cloudflare/cloudflare"
+      version = "~> 5.6.0"
+    }
+    http = {
+      source = "hashicorp/http"
+      version = "~> 3.4.2"
+  }
+}
+
+data "http" "github" {
+  url = "https://api.github.com/meta"
+}
+
+data "cloudflare_account" "siguiente" {
+  filter {
+    name = "Siguiente"
+  }
+}
+
+locals {
+  git = jsondecode(data.http.github.response_body)["git"]
+}
+
+resource "cloudflare_zero_trust_list" "github_git_cidr" {
+  account_id = data.cloudflare_account.siguiente.account_id
+  name = "Github Git IP"
+  description = ""
+  type = "IP"
+  items = [ ip in local.git : { value = ip } ]
+}
